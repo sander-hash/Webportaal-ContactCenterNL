0f304d87a554625da47083126ffac4d10a6ffc67
sander-hash <69810469+sander-hash@users.noreply.github.com>
2021-09-14 23:16:36 +0200

    Betaalpagina toegevoegd

---
+++ webportaalCC/resources/js/Pages/Betalenfilm1.vue
@@ -0,0 +1,48 @@
+
+<template>
+ <div>
+        <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Betalen
+            </h2>
+
+        </template>
+
+
+
+
+     <template #footer>
+
+         
+
+         </template>
+
+    </app-layout>
+
+
+ </div>
+ 
+
+
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import Welcome from '@/Jetstream/Welcome.vue'
+
+    const rankingsBody = document.querySelector("#rankings-table > tbody");
+
+    console.log(rankingsBody);
+
+
+    export default {
+      props:['sd'],
+        components: {
+          
+            AppLayout,
+            Welcome,
+        },
+    }
+    
+</script>
